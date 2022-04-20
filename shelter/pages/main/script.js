7fa4504c116d2931092ee7c05a7f579086258676
Anastasia <nastia.shrai@gmail.com>
2022-04-20 19:49:35 +0300

    refactor: refactor burger menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,12 +1,30 @@
 const burgerIcon = document.querySelector('.burger__menu');
+const headerLogo = document.querySelector('.header__logo');
 const navigationMenu = document.querySelector('.menu__list');
 const navigationItems = document.querySelectorAll('.menu__list-item');
-
-if (burgerIcon) {
-  burgerIcon.addEventListener('click', open);
-}
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
 
 function open() {
-  navigationMenu.classList.toggle('show');
-  burgerIcon.classList.toggle('active');
+  navigationMenu.classList.remove('hide');
+  navigationMenu.classList.add('show');
+  headerLogo.classList.add('header__logo-burger');
+  burgerIcon.classList.add('active');
+  document.body.style.overflow = 'hidden';
+  document.body.prepend(overlay);
 }
+
+function close() {
+  navigationMenu.classList.remove('show');
+  navigationMenu.classList.add('hide');
+  headerLogo.classList.remove('header__logo-burger');
+  burgerIcon.classList.remove('active');
+  overlay.remove();
+  document.body.style.overflow = 'scroll';
+}
+
+burgerIcon.addEventListener('click', () => {
+  navigationMenu.classList.contains('show') ? close() : open();
+});
+
+overlay.addEventListener('click', close);
